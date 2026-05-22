53bf0f48272ed768e205cc36bfbfdbe3197be5b4
dl-84 <72781732+dl-84@users.noreply.github.com>
2026-05-22 12:47:30 +0200

    Add empty LicenseTable control project

---
+++ Avalonia/Controls/LicenseTable/LicenseTableControl.axaml.cs
@@ -0,0 +1,15 @@
+using Avalonia.Controls;
+
+namespace Controls.LicenseTable;
+
+/// <inheritdoc />
+public partial class LicenseTableControl : UserControl
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="LicenseTableControl"/> class.
+    /// </summary>
+    public LicenseTableControl()
+    {
+        InitializeComponent();
+    }
+}
